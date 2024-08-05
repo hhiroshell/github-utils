f804a08c89daeaa710a9f91aed4b76aaa7524018
Hiroshi Hayakawa <hhiroshell@gmail.com>
2024-08-05 20:53:08 +0900

    some small fixes

--- contrib-stats.sh
+++ contrib-stats.sh
@@ -17,7 +17,7 @@ while getopts ":r:u:h" optKey; do
     u)
         user="${OPTARG}"
         ;;
-    '-h'|'--help'|* )
+    h|*)
       usage
       exit 2
       ;;
@@ -29,7 +29,7 @@ if [ -z "${repository}" ] || [ -z "${user}" ]; then
     exit 2
 fi
 
-echo "Repository: "${repository}", User: ${user}"
+echo "Repository: ${repository}, User: ${user}"
 
 if ! gh auth status > /dev/null 2>&1; then
     echo "GitHub CLI is not authenticated. Please run 'gh auth login' to authenticate."
@@ -42,7 +42,7 @@ for n in $(
         --jq "[.[] | select(.author.login == \"${user}\")] | length" \
         --paginate
 ); do
-    commits_count=$(($commits_count + n))
+    commits_count=$((commits_count + n))
 done
 
 echo "Counting pull requests..." >&2
@@ -51,7 +51,7 @@ for n in $(
         --jq "[.[] | select(.user.login == \"${user}\")] | length" \
         --paginate
 ); do
-    pulls_count=$(($pulls_count + n))
+    pulls_count=$((pulls_count + n))
 done
 
 echo "Counting issues..." >&2
@@ -60,12 +60,10 @@ for n in $(
         --jq "[.[] | select(.user.login == \"${user}\") | select(.pull_request | not)] | length" \
         --paginate
 ); do
-    issues_count=$(($issues_count + n))
+    issues_count=$((issues_count + n))
 done
-
 
 echo "==== RESULTS ===="
 echo "Commits: ${commits_count}"
 echo "Pull Requests: ${pulls_count}"
 echo "Issues: ${issues_count}"
-
